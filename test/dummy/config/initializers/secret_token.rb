1a07535806c6e8de0c24a3a3c749ec4ee7292eda
Gil Magno <gilmagno@gmail.com>
2012-04-26 17:30:53 -0300

    tests

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '4aa69fba28750f6d761fef65b50eb0218c1195c0b0a29bd2bd4c2196fcf5ad5d3697c9e8d3905fe6abaab9221c11087c7a038f121e3f802075e116fa25ba7089'
---
+++ test/uni_person_data_br_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UniPersonDataBrTest < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, UniPersonDataBr
+  end
+end
